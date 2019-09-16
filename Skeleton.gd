d62d0fab7990b8358667eb67f7fb92b4dd7883d6
dw3nt <rick@dualwieldent.com>
2019-09-15 20:07:52 -0700

    skeleton script for basic movement

---
+++ Skeleton.gd
@@ -0,0 +1,22 @@
+extends KinematicBody2D
+
+const MAX_RUN_SPEED = 200
+const ACCELERATION = 20
+
+var motion = Vector2()
+
+func _physics_process(delta):
+	if should_stop():
+		motion.x = lerp(motion.x, 0, 0.25)
+	elif Input.is_action_pressed("move_left"):
+		motion.x = max(motion.x - ACCELERATION, -MAX_RUN_SPEED)
+		$Sprite.flip_h = true
+	elif Input.is_action_pressed("move_right"):
+		motion.x = min(motion.x + ACCELERATION, MAX_RUN_SPEED)
+		$Sprite.flip_h = false
+		
+	move_and_slide(motion)
+	
+	
+func should_stop():
+	return (!Input.is_action_pressed("move_left") && !Input.is_action_pressed("move_right")) || (Input.is_action_pressed("move_left") && Input.is_action_pressed("move_right"))
