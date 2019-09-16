3c8116e2b6eb9f2dc609f2caabb471645a5991f6
dw3nt <rick@dualwieldent.com>
2019-09-15 21:27:42 -0700

    skeleton rolling state

--- Skeleton.gd
+++ Skeleton.gd
@@ -9,6 +9,15 @@ onready var state = State.MOVE
 var motion = Vector2()
 
 func _physics_process(delta):
+	if Input.is_action_just_pressed("roll"):
+		state = State.ROLL
+	
+	match state:
+		State.MOVE:
+			process_move()
+		State.ROLL:
+			process_roll()
+	
 	move_and_slide(motion)
 	
 	
@@ -26,7 +35,21 @@ func process_move():
 		$Sprite.flip_h = false
 	else:
 		$Anim.play("idle")
+		
+
+func process_roll():
+	var x_direction = 1
+	if $Sprite.flip_h:
+		x_direction = -1
+	
+	$Anim.play("roll")
+	motion.x = 200 * x_direction
 
 	
 func should_stop():
 	return (!Input.is_action_pressed("move_left") && !Input.is_action_pressed("move_right")) || (Input.is_action_pressed("move_left") && Input.is_action_pressed("move_right"))
+
+
+func _on_Anim_animation_finished(anim_name):
+	if anim_name == "roll":
+		state = State.MOVE
