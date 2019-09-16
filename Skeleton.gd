254a9b18d0d653a111374b248fe81f59382c2c5e
dw3nt <rick@dualwieldent.com>
2019-09-15 20:47:23 -0700

    adjusted walls with skeleton run animation

--- Skeleton.gd
+++ Skeleton.gd
@@ -8,12 +8,17 @@ var motion = Vector2()
 func _physics_process(delta):
 	if should_stop():
 		motion.x = lerp(motion.x, 0, 0.25)
+		$Anim.play("idle")
 	elif Input.is_action_pressed("move_left"):
 		motion.x = max(motion.x - ACCELERATION, -MAX_RUN_SPEED)
+		$Anim.play("run")
 		$Sprite.flip_h = true
 	elif Input.is_action_pressed("move_right"):
 		motion.x = min(motion.x + ACCELERATION, MAX_RUN_SPEED)
+		$Anim.play("run")
 		$Sprite.flip_h = false
+	else:
+		$Anim.play("idle")
 		
 	move_and_slide(motion)
 	
