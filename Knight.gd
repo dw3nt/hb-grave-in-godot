fdc0b6496f222e9f828f3a1bf06abbad29885877
dw3nt <rick@dualwieldent.com>
2019-09-19 08:22:26 -0700

    primitive knight chase

--- Knight.gd
+++ Knight.gd
@@ -2,18 +2,47 @@ extends KinematicBody2D
 
 enum State { IDLE, CHASE }
 
-const MAX_MOVE_SPEED = 125
-const ACCELERATION = 20
+const MAX_MOVE_SPEED = 50
+const ACCELERATION = 10
 
 var motion = Vector2()
 
 onready var state = State.IDLE
 
 func _physics_process(delta):
+	# may be a better way to tell enemies player doesn't exist (or is dead) cuz this is bad practice and inefficent
+	var skeleton = get_tree().get_root().find_node("Skeleton", true, false)
+	if skeleton == null:
+		state = State.IDLE
+	else:
+		state = State.CHASE
+	
 	match state:
 		State.IDLE:
-			print("idle")
+			process_idle()
 		State.CHASE:
-			print("chase")
+			process_chase(skeleton)
 			
 	move_and_slide(motion)
+	
+
+func process_idle():
+	$Anim.play("idle")
+	motion.x = lerp(motion.x, 0, 0.25)
+	
+	
+func process_chase(chase):
+	var x_scale = sign(chase.position.x - position.x)
+	if x_scale == 0:
+		x_scale = 1
+		
+	if x_scale > 0:
+		motion.x = min(motion.x + ACCELERATION, MAX_MOVE_SPEED)
+	else:
+		motion.x = max(motion.x - ACCELERATION, -MAX_MOVE_SPEED)
+		
+	$Anim.play("walk")
+	$Flippable.scale.x = x_scale
+			
+			
+			
