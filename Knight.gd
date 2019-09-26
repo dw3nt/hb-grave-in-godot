de540e1a04439d170c8b1fda7bbbc4f8b6103975
dw3nt <rick@dualwieldent.com>
2019-09-26 08:12:39 -0700

    add attack animation to knight

--- Knight.gd
+++ Knight.gd
@@ -5,24 +5,26 @@ enum State { IDLE, CHASE, ATTACK }
 const MAX_MOVE_SPEED = 50
 const ACCELERATION = 10
 
-var motion = Vector2()
-
 onready var state = State.CHASE
+onready var skeleton = get_tree().get_root().find_node("Skeleton", true, false)
+
+var motion = Vector2()
+var inAttackRange = false
 
 func _physics_process(delta):
 	# may be a better way to tell enemies player doesn't exist (or is dead) cuz this is bad practice and inefficent
-	var skeleton = get_tree().get_root().find_node("Skeleton", true, false)
 	if skeleton == null:
 		state = State.IDLE
 	
-	set_face_direction(skeleton)
 	match state:
 		State.IDLE:
+			set_face_direction(skeleton)
 			process_idle()
 		State.CHASE:
+			set_face_direction(skeleton)
 			process_chase(skeleton)
 		State.ATTACK:
-			process_idle()
+			process_attack()
 			
 	move_and_slide(motion)
 	
@@ -40,6 +42,11 @@ func process_chase(chase):
 		motion.x = max(motion.x - ACCELERATION, -MAX_MOVE_SPEED)
 		
 	$Anim.play("walk")
+	
+	
+func process_attack():
+	$Anim.play("attack")
+	motion.x = lerp(motion.x, 0, 0.25)
 			
 
 func set_face_direction(faceTowards):
@@ -52,9 +59,18 @@ func set_face_direction(faceTowards):
 
 func _on_AttackDetect_body_entered(body):
 	if body.get_name() == "Skeleton":
-		state = State.IDLE
+		inAttackRange = true
+		state = State.ATTACK
 
 
 func _on_AttackDetect_body_exited(body):
 	if body.get_name() == "Skeleton":
-		state = State.CHASE
+		inAttackRange = false
+
+
+func _on_Anim_animation_finished(anim_name):
+	if anim_name == "attack":
+		if inAttackRange:
+			set_face_direction(skeleton)
+		else:
+			state = State.CHASE
