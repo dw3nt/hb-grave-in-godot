e21536ebc41d2e3073cdbe62653eb94a86823c94
dw3nt <rick@dualwieldent.com>
2019-10-05 21:02:59 -0700

    added kills and level to UI

--- UI.gd
+++ UI.gd
@@ -1,5 +1,7 @@
 extends CanvasLayer
 
+onready var HPBar = $MarginContainer/VBoxContainer/Row1/HPBar
+
 func _on_Skeleton_hp_changed(hp):
-	$Tween.interpolate_property($HPBar, "value", $HPBar.value, hp, 0.25, Tween.TRANS_LINEAR, Tween.EASE_OUT, 0)
+	$Tween.interpolate_property(HPBar, "value", HPBar.value, hp, 0.25, Tween.TRANS_LINEAR, Tween.EASE_OUT, 0)
 	$Tween.start()
