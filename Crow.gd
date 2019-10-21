95b706f5b809798ce595d2cc90943088b660b343
dw3nt <rick@dualwieldent.com>
2019-10-21 07:40:47 -0700

    set crow direction on init

--- Crow.gd
+++ Crow.gd
@@ -18,6 +18,18 @@ var expAmount = 1
 onready var state = State.CHASE
 onready var skeleton = get_tree().get_root().find_node("Skeleton", true, false)
 
+func _ready():
+	var dir = null
+	if skeleton == null:
+		dir = rand_range(-1, 1)
+	else:
+		dir = skeleton.position.x - position.x
+		
+	if dir < 0:
+		isFlipped = true
+		scale.x = -1		
+
+
 func _physics_process(delta):
 	# may be a better way to tell enemies player doesn't exist (or is dead) cuz this seems like bad practice / inefficent
 	if skeleton == null:
