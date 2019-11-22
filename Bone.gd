eb8cbf66e293633d00680a37042c743569cc54a2
dw3nt <rick@dualwieldent.com>
2019-11-22 08:00:06 -0700

    setup bone timer for game over transition

--- Bone.gd
+++ Bone.gd
@@ -13,6 +13,7 @@ func _ready():
 	$Sprite.frame = boneFrame
 	if boneFrame == SWORD_FRAME:	# special angle for sword
 		rotation_degrees = SWORD_ANGLE
+		add_game_over_timer()
 	else:
 		rotation_degrees = rand_range(0, 360)
 		
@@ -27,3 +28,19 @@ func _physics_process(delta):
 	if global_position.y < GROUND_LIMIT:
 		motion.y += GRAVITY
 		move_and_slide(motion)
+		
+
+func add_game_over_timer():
+	var timer = Timer.new()
+	timer.set_one_shot(true)
+	timer.set_timer_process_mode(1)		# enumerated - 1 = TIMER_PROCESS_IDLE
+	timer.set_wait_time(2)
+	timer.connect("timeout", self, "_timer_timeout")
+	timer.start()
+	add_child(timer)
+	
+
+func _timer_timeout():
+	print("bone timeout")	# transition to game over screen after saving score(s)
+	
+	
